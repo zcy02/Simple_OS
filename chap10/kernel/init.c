280da33b38744a014c15c73d56adca6aef4cd106
AnLxn <928761922@qq.com>
2023-05-07 14:19:09 +0800

    chap10 done

---
+++ chap10/kernel/init.c
@@ -0,0 +1,19 @@
+#include "init.h"
+#include "print.h"
+#include "interrupt.h"
+#include "timer.h"
+#include "memory.h"
+#include "thread.h"
+#include "console.h"
+#include "keyboard.h"
+
+/* 负责初始化所有模块 */
+void init_all(){
+        put_str("init_all\n");
+        idt_init();             // 初始化 中断
+        timer_init();           // 初始化 PIT
+	mem_init();		// 初始化内存池
+        thread_init();
+        console_init();
+        keyboard_init();
+}
